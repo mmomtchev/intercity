d5b487fa0e4f5946f1677a8bcdebf4853a79420f
Momtchil Momtchev <momtchil@momtchev.com>
2021-07-10 15:01:00 +0200

    use the new vsimem instead of a tmp file

--- src/jpeg.js
+++ src/jpeg.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const gdal = require('gdal-async');
-const fs = require('fs');
 const Format = require('./format').Format;
 
 let uid = 1;
@@ -12,11 +11,9 @@ class JPEG extends Format {
 
     async produce(ds) {
         const jpegDriver = gdal.drivers.get('JPEG');
-        const tmpName = `tmp${uid}.jpeg`;
+        const tmpName = `/vsimem/${uid++}.jpeg`;
         await jpegDriver.createCopyAsync(tmpName, ds)
-        //const raw = Buffer.alloc(size * size * 3, 0);
-        //const png = await gdal.openAsync(raw, 'w', 'png', size, size, 3, gdal.GDT_Byte);
-        const raw = await fs.promises.readFile(tmpName);
+        const raw = gdal.vsimem.release(tmpName);
         return raw;
     }
 }
--- src/png.js
+++ src/png.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const gdal = require('gdal-async');
-const fs = require('fs');
 const Format = require('./format').Format;
 
 let uid = 1;
@@ -12,11 +11,9 @@ class PNG extends Format {
 
     async produce(ds) {
         const pngDriver = gdal.drivers.get('PNG');
-        const tmpName = `tmp${uid}.png`;
+        const tmpName = `/vsimem/${uid++}.png`;
         await pngDriver.createCopyAsync(tmpName, ds)
-        //const raw = Buffer.alloc(size * size * 3, 0);
-        //const png = await gdal.openAsync(raw, 'w', 'png', size, size, 3, gdal.GDT_Byte);
-        const raw = await fs.promises.readFile(tmpName);
+        const raw = gdal.vsimem.release(tmpName);
         return raw;
     }
 }
