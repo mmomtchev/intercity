b3408fd3e2507573f2f7b1a305005178c798fd5f
Momtchil Momtchev <momtchil@momtchev.com>
2021-10-15 21:16:12 +0200

    first unit tests

---
+++ test/libtest.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const fs = require('fs');
+const path = require('path')
+const { PNG } = require('pngjs');
+const pixelmatch = require('pixelmatch');
+const proj4 = require('proj4');
+
+function matchPNGtoURL(base, url, png, size) {
+    return () => chai.request(base)
+        .get(url)
+        .then((data) => {
+            expect(data).to.have.status(200);
+            const actual = PNG.sync.read(data.body);
+            expect(actual.width).to.equal(size);
+            expect(actual.height).to.equal(size);
+            const expected = PNG.sync.read(fs.readFileSync(path.join(__dirname, 'data', png)));
+            expect(pixelmatch(actual.data, expected.data, null, size, size)).below(1);
+        })
+}
+
+function projBox(sproj, tproj, coords) {
+    if (coords.length != 4) throw new Error('A bbox must have 4 coordinates');
+    return proj4(sproj, tproj, coords.slice(0, 2)).concat(proj4(sproj, tproj, coords.slice(2, 4)));
+}
+
+module.exports = {
+    matchPNGtoURL,
+    projBox,
+    base: 'http://localhost:3000'
+};
---
+++ test/wmts.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const { base, matchPNGtoURL, projBox } = require('./libtest');
+
+describe('WMS', () => {
+    describe('GetCapabilities', () => {
+        it('should return a valid response', () => {
+            chai.request('http://localhost:3000')
+                .get('/wms?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities')
+                .then((data) => {
+                    expect(data).to.have.status(200);
+                })
+        })
+    })
+
+    describe('GetMap', () => {
+        it('should return native tiles',
+            matchPNGtoURL(base, '/wmts/tile/stripes:yellow/PNG/GoogleCRS84Quad/4/8/2.png', 'wmts_googleCRS84quad.png', 256)
+        )
+
+        it('should return reprojected tiles',
+            matchPNGtoURL(base, '/wmts/tile/stripes:yellow/PNG/GoogleCRS84Quad/4/8/2.png', 'wmts_googleCRS84quad.png', 256)
+        )
+
+    })
+})
